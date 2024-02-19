c1e6ab5eb22099339c4d3873842157d7a228d067
Fukulotszn <miguel.daryll.bscs2021@gmail.com>
2024-02-19 20:01:02 +0800

    navarro

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="index.css" />
+    <title>T-Sync</title>
+  </head>
+  <body>
+    <nav>
+    <div class = "nav_logo">
+<img src = "{{('logo.png')}}" class  =" ">
+ </div>
+      <ul class="nav__links">
+        <li class="link"><a href="login">Log In</a></li>
+        <li class="link"><a href="login" class="nav__btn">Sign Up</a></li>
+      </ul>
+    </nav>
+    <section class="container">
+      <div class="content__container">
+        <h1>
+        Streamline Workflows,<br />
+          <span class="heading__1">Enhance Communication,</span><br />
+          <span class="heading__2">Achieve Success</span>
+        </h1>
+        <p>
+        Experience the power of T-Sync,
+         a robust Project Management System designed to meet the demands of efficient task management, 
+         seamless communication, and collaborative teamwork.
+        </p>
+</div>
+        <div class="image__container" style="margin-top: 190px;">
+        <img src="image 1.png" alt="header" />
+        </div>
+    </section>
+  </body>
+</html>
